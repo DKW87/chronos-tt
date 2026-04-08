e2c99fb00a1498556007be5dd932c81bc1790682
DKW87 <dannykwant@gmail.com>
2026-04-08 20:27:05 +0200

    feat(deps): add lombok to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.github.dkw87.chronostt {
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
+    requires static lombok;
 
     opens com.github.dkw87.chronostt to javafx.fxml;
     exports com.github.dkw87.chronostt;
