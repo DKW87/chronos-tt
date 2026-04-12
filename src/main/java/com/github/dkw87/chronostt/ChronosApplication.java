3fff104ea4a23adc1280738b6365bbb9d22ef7d7
DKW87 <dannykwant@gmail.com>
2026-04-12 19:12:12 +0200

    refactor(ChronosApplication): adjust to SceneManager and becomes static carrier of SceneManager

--- src/main/java/com/github/dkw87/chronostt/ChronosApplication.java
+++ src/main/java/com/github/dkw87/chronostt/ChronosApplication.java
@@ -1,32 +1,38 @@
 package com.github.dkw87.chronostt;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 
 public class ChronosApplication extends Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChronosApplication.class);
+    private static SceneManager sceneManager;
+    private static Stage primaryStage;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         LOGGER.info("Chronos-TT starting...");
-        FXMLLoader fxmlLoader = new FXMLLoader(ChronosApplication.class.getResource("view/SettingsView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 250, 300);
-        stage.setTitle("Chronos-TT");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage = stage;
+        getSceneManager().showSettingsView();
+        primaryStage.setTitle("Chronos-TT");
+        primaryStage.show();
         LOGGER.info("Chronos-TT started.");
     }
 
     @Override
     public void stop() {
         LOGGER.info("Chronos-TT stopping...");
+    }
+
+    public static SceneManager getSceneManager() {
+        if (sceneManager == null) {
+            LOGGER.info("Initializing SceneManager...");
+            sceneManager = new SceneManager(primaryStage);
+        }
+        return sceneManager;
     }
 
 }
