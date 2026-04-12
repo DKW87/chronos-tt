1afd79109a0c2285cf73b6724381432d9bab293a
DKW87 <dannykwant@gmail.com>
2026-04-12 20:30:44 +0200

    refactor(SceneManager): now refactored into StageManager and once again is it's own static holder

--- src/main/java/com/github/dkw87/chronostt/ChronosApplication.java
+++ src/main/java/com/github/dkw87/chronostt/ChronosApplication.java
@@ -9,30 +9,18 @@ import org.slf4j.LoggerFactory;
 public class ChronosApplication extends Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChronosApplication.class);
-    private static SceneManager sceneManager;
-    private static Stage primaryStage;
+    private static final StageManager STAGE_MANAGER = StageManager.getInstance();
 
     @Override
     public void start(Stage stage) {
         LOGGER.info("Chronos-TT starting...");
-        primaryStage = stage;
-        getSceneManager().showSettingsView();
-        primaryStage.setTitle("Chronos-TT");
-        primaryStage.show();
+        STAGE_MANAGER.showSettingsView();
         LOGGER.info("Chronos-TT started.");
     }
 
     @Override
     public void stop() {
         LOGGER.info("Chronos-TT stopping...");
-    }
-
-    public static SceneManager getSceneManager() {
-        if (sceneManager == null) {
-            LOGGER.info("Initializing SceneManager...");
-            sceneManager = new SceneManager(primaryStage);
-        }
-        return sceneManager;
     }
 
 }
---
+++ src/main/java/com/github/dkw87/chronostt/StageManager.java
@@ -0,0 +1,77 @@
+package com.github.dkw87.chronostt;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class StageManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StageManager.class);
+
+    private final Stage settingsStage = new Stage();
+    private final Stage trackingStage = new Stage();
+
+    private StageManager() {
+        LOGGER.info("Constructing stages...");
+        constructSettingsStage();
+        constructTrackingStage();
+    }
+
+    private void constructSettingsStage() {
+        settingsStage.initStyle(StageStyle.DECORATED);
+        getScene("view/SettingsView.fxml",  settingsStage);
+        settingsStage.setWidth(250);
+        settingsStage.setHeight(320);
+        settingsStage.setTitle("Chronos-TT: Settings");
+    }
+
+    private void constructTrackingStage() {
+        trackingStage.initStyle(StageStyle.UNDECORATED);
+        getScene("view/TrackingView.fxml",  trackingStage);
+        trackingStage.setWidth(250);
+        trackingStage.setHeight(50);
+        trackingStage.setAlwaysOnTop(true);
+        trackingStage.setTitle("Chronos-TT: Tracking Time...");
+    }
+
+    private FXMLLoader getScene(String fxml, Stage stage) {
+        Scene scene;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Parent root = loader.load();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            return loader;
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return null;
+        }
+    }
+
+    public void showSettingsView() {
+        LOGGER.info("Switching to SettingsView");
+        trackingStage.hide();
+        settingsStage.show();
+    }
+
+    public void showTrackingView() {
+        LOGGER.info("Switching to TrackingView");
+        settingsStage.hide();
+        trackingStage.show();
+    }
+
+    public static StageManager getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final StageManager INSTANCE = new StageManager();
+    }
+
+}
