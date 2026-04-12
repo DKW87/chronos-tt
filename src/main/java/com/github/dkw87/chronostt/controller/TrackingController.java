0759eb19f538fff4da1ac4cbc2d12d3c1370c5e7
DKW87 <dannykwant@gmail.com>
2026-04-12 15:19:32 +0200

    feat(controller): add init TrackingController

---
+++ src/main/java/com/github/dkw87/chronostt/controller/TrackingController.java
@@ -0,0 +1,14 @@
+package com.github.dkw87.chronostt.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class TrackingController {
+
+    @FXML
+    public ComboBox projectComboBox;
+
+    @FXML
+    public void stopTracking() {
+    }
+}
