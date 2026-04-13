fd5073c6f5cba4bc825c451bc6077acb30e2add4
DKW87 <dannykwant@gmail.com>
2026-04-13 20:29:25 +0200

    feat(TrackingController): add draggable

--- src/main/java/com/github/dkw87/chronostt/controller/TrackingController.java
+++ src/main/java/com/github/dkw87/chronostt/controller/TrackingController.java
@@ -1,14 +1,38 @@
 package com.github.dkw87.chronostt.controller;
 
+import com.github.dkw87.chronostt.StageManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
 
 public class TrackingController {
 
     @FXML
-    public ComboBox projectComboBox;
+    private HBox root;
 
     @FXML
-    public void stopTracking() {
+    public ComboBox projectComboBox;
+
+    private double xOffset;
+    private double yOffset;
+
+    @FXML
+    public void initialize() {
+        draggableStage();
     }
+
+    private void draggableStage() {
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            StageManager.getInstance().getTrackingStage().setX(event.getScreenX() - xOffset);
+            StageManager.getInstance().getTrackingStage().setY(event.getScreenY() - yOffset);
+        });
+    }
+
+    @FXML
+    public void stopTracking() {}
+
 }
