ea920f84e7eb7cb3fbd4e0ae36513f545ea096d0
DKW87 <dannykwant@gmail.com>
2026-04-16 20:22:51 +0200

    feat(repository): make MemoryRepository and StorageRepository singleton

--- src/main/java/com/github/dkw87/chronostt/repository/memory/MemoryRepository.java
+++ src/main/java/com/github/dkw87/chronostt/repository/memory/MemoryRepository.java
@@ -1,4 +1,13 @@
 package com.github.dkw87.chronostt.repository.memory;
 
 public class MemoryRepository {
+
+    public static MemoryRepository getInstance() {
+        return MemoryRepository.SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final MemoryRepository INSTANCE = new MemoryRepository();
+    }
+    
 }
--- src/main/java/com/github/dkw87/chronostt/repository/storage/StorageRepository.java
+++ src/main/java/com/github/dkw87/chronostt/repository/storage/StorageRepository.java
@@ -1,4 +1,14 @@
 package com.github.dkw87.chronostt.repository.storage;
 
+
 public class StorageRepository {
+
+    public static StorageRepository getInstance() {
+        return StorageRepository.SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final StorageRepository INSTANCE = new StorageRepository();
+    }
+    
 }
