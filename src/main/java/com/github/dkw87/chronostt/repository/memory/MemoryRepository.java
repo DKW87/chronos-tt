939a57816606d38c45f6094a1398759d0a1f2d5f
DKW87 <dannykwant@gmail.com>
2026-04-18 19:07:58 +0200

    feat(MemoryRepository): add private variables to manage

--- src/main/java/com/github/dkw87/chronostt/repository/memory/MemoryRepository.java
+++ src/main/java/com/github/dkw87/chronostt/repository/memory/MemoryRepository.java
@@ -1,6 +1,24 @@
 package com.github.dkw87.chronostt.repository.memory;
 
+import com.github.dkw87.chronostt.model.DayEntry;
+import com.github.dkw87.chronostt.model.Project;
+import com.github.dkw87.chronostt.model.Settings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class MemoryRepository {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MemoryRepository.class);
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private volatile Settings settings;
+    private volatile List<Project> projects;
+    private volatile DayEntry today;
+    private volatile List<DayEntry> allDays;
 
     public static MemoryRepository getInstance() {
         return MemoryRepository.SingletonHolder.INSTANCE;
