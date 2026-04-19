659c97300bfd5c43cee8860079fe72f1d4ef9c93
DKW87 <dannykwant@gmail.com>
2026-04-19 14:48:38 +0200

    feat(StorageRepository): init

--- src/main/java/com/github/dkw87/chronostt/repository/storage/StorageRepository.java
+++ src/main/java/com/github/dkw87/chronostt/repository/storage/StorageRepository.java
@@ -1,7 +1,75 @@
 package com.github.dkw87.chronostt.repository.storage;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class StorageRepository {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StorageRepository.class);
+    private static final Runnable SHUTDOWN_TASK = () -> {};
+    private static final String THREAD_NAME = "StorageRepositoryThread";
+
+    private static final String USER_HOME = System.getProperty("user.home");
+    private static final String APP_DIR = ".chronos-tt";
+    private static final Path PATH = Paths.get(USER_HOME, APP_DIR);
+    private static final String SETTINGS_FILE = "chronos-settings.json";
+    private static final String PROJECTS_FILE = "chronos-projects.json";
+    private static final String TRACKING_FILE = "chronos-tracking.json";
+
+    private final BlockingQueue<Runnable> queue;
+    private final ObjectMapper objectMapper;
+    
+    private StorageRepository(){
+        LOG.info("Initializing StorageRepository...");
+        queue = new LinkedBlockingQueue<>();
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        start();
+    }
+
+    private void start() {
+        Thread storageRepositoryThread = new Thread(() -> {
+            LOG.info("{} started", THREAD_NAME);
+            while (true) {
+                try {
+                    Runnable task = queue.take();
+
+                    if (task == SHUTDOWN_TASK) {
+                        LOG.info("{} completed all queued tasks", THREAD_NAME);
+                        break;
+                    }
+
+                    task.run();
+                } catch (InterruptedException e) {
+                    LOG.error("{} interrupted, {} tasks left in queue", THREAD_NAME, queue.size(), e);
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+            LOG.info("{} stopped", THREAD_NAME);
+            shutdown();
+        }, THREAD_NAME);
+        storageRepositoryThread.setDaemon(true);
+        storageRepositoryThread.start();
+    }
+
+    public void stop() {
+        LOG.info("StorageRepository shutting down...");
+        queue.add(SHUTDOWN_TASK);
+    }
+
+    private void shutdown() {
+        LOG.info("StorageRepository has stopped");
+        // do something maybe
+    }
 
     public static StorageRepository getInstance() {
         return StorageRepository.SingletonHolder.INSTANCE;
