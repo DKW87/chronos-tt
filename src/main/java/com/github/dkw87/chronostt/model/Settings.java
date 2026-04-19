9ba9a423bc06f56f0fd2acad5922fd864fbb4bbc
DKW87 <dannykwant@gmail.com>
2026-04-19 15:12:37 +0200

    feat(Settings): add @Builder

--- src/main/java/com/github/dkw87/chronostt/model/Settings.java
+++ src/main/java/com/github/dkw87/chronostt/model/Settings.java
@@ -1,10 +1,12 @@
 package com.github.dkw87.chronostt.model;
 
 import com.github.dkw87.chronostt.enumeration.TimeScale;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
 @RequiredArgsConstructor
