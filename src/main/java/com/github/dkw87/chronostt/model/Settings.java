d8b7ec4d0a0b1b3b1ce8a43645096c232ae37388
DKW87 <dannykwant@gmail.com>
2026-04-21 20:34:49 +0200

    refactor(Settings): add method for getting default Settings

--- src/main/java/com/github/dkw87/chronostt/model/Settings.java
+++ src/main/java/com/github/dkw87/chronostt/model/Settings.java
@@ -17,4 +17,15 @@ public class Settings {
     Boolean notifyOvertime;
     Boolean aggregateProjectHours;
 
+    public static Settings defaults() {
+        return Settings.builder()
+                .daysWeek(5)
+                .hoursDaily(8)
+                .lastSelectedProject(null)
+                .timeScale(TimeScale.THIRTY_MINUTES)
+                .notifyOvertime(false)
+                .aggregateProjectHours(true)
+                .build();
+    }
+
 }
