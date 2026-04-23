efdc4bb895d0d2e0ca04d727590644e7bba6131f
DKW87 <dannykwant@gmail.com>
2026-04-23 19:50:42 +0200

    feat(DataService): add getProjects()

--- src/main/java/com/github/dkw87/chronostt/service/DataService.java
+++ src/main/java/com/github/dkw87/chronostt/service/DataService.java
@@ -1,11 +1,19 @@
 package com.github.dkw87.chronostt.service;
 
+import com.github.dkw87.chronostt.model.Project;
+import com.github.dkw87.chronostt.repository.memory.MemoryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class DataService {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataService.class);
+
+    public List<Project> getProjects() {
+        return MemoryRepository.getInstance().getProjects();
+    }
 
     public static DataService getInstance() {
         return DataService.SingletonHolder.INSTANCE;
