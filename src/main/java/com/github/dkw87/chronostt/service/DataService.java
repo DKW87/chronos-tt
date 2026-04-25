cd2055e6deeb5f44ee99d7ddcd07cdf5848be530
DKW87 <dannykwant@gmail.com>
2026-04-25 20:13:16 +0200

    feat(DataService): add getLastSelectedProject() and storeLastSelectedProject()

--- src/main/java/com/github/dkw87/chronostt/service/DataService.java
+++ src/main/java/com/github/dkw87/chronostt/service/DataService.java
@@ -2,6 +2,7 @@ package com.github.dkw87.chronostt.service;
 
 import com.github.dkw87.chronostt.enumeration.TimeScale;
 import com.github.dkw87.chronostt.model.Project;
+import com.github.dkw87.chronostt.model.Settings;
 import com.github.dkw87.chronostt.repository.memory.MemoryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,16 @@ import java.util.List;
 public class DataService {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataService.class);
+
+    public Project getLastSelectedProject() {
+        return MemoryRepository.getInstance().getSettings().getLastSelectedProject();
+    }
+
+    public void storeLastSelectedProject(Project project) {
+        Settings settings = MemoryRepository.getInstance().getSettings();
+        settings.setLastSelectedProject(project);
+        MemoryRepository.getInstance().submitSettings(settings);
+    }
 
     public List<Project> getProjects() {
         return MemoryRepository.getInstance().getProjects();
