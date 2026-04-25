78a4276dd8487b1bcdffe20b26f146ae8f8c55ba
DKW87 <dannykwant@gmail.com>
2026-04-25 20:44:29 +0200

    feat(SettingsService): add remaining methods to store settings

--- src/main/java/com/github/dkw87/chronostt/service/SettingsService.java
+++ src/main/java/com/github/dkw87/chronostt/service/SettingsService.java
@@ -16,20 +16,50 @@ public class SettingsService {
         return MemoryRepository.getInstance().getSettings().getDaysWeek();
     }
 
+    public void storeDaysWeek(int daysWeek) {
+        Settings settings = MemoryRepository.getInstance().getSettings();
+        settings.setDaysWeek(daysWeek);
+        MemoryRepository.getInstance().submitSettings(settings);
+    }
+
     public int getHoursDaily() {
         return MemoryRepository.getInstance().getSettings().getHoursDaily();
+    }
+
+    public void storeHoursDaily(int hoursDaily) {
+        Settings settings = MemoryRepository.getInstance().getSettings();
+        settings.setHoursDaily(hoursDaily);
+        MemoryRepository.getInstance().submitSettings(settings);
     }
 
     public TimeScale getTimeScale() {
         return MemoryRepository.getInstance().getSettings().getTimeScale();
     }
 
+    public void storeTimeScale(TimeScale timeScale) {
+        Settings settings = MemoryRepository.getInstance().getSettings();
+        settings.setTimeScale(timeScale);
+        MemoryRepository.getInstance().submitSettings(settings);
+    }
+
     public boolean getNotifyOvertime() {
         return MemoryRepository.getInstance().getSettings().getNotifyOvertime();
     }
 
+    public void storeNotifyOvertime(boolean notifyOvertime) {
+        Settings settings = MemoryRepository.getInstance().getSettings();
+        settings.setNotifyOvertime(notifyOvertime);
+        MemoryRepository.getInstance().submitSettings(settings);
+    }
+
     public boolean getAggragateProjectHours() {
         return MemoryRepository.getInstance().getSettings().getAggregateProjectHours();
+    }
+
+    public void storeAggragateProjectHours(boolean aggragateProjectHours) {
+        Settings settings = MemoryRepository.getInstance().getSettings();
+        settings.setAggregateProjectHours(aggragateProjectHours);
+        MemoryRepository.getInstance().submitSettings(settings);
     }
 
     public Project getLastSelectedProject() {
