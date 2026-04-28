abf77cfb76c115fdb911bd68decde810603cba24
DKW87 <dannykwant@gmail.com>
2026-04-28 19:52:52 +0200

    feat(view): init ManageProjectsView.fxml and associated controller

---
+++ src/main/java/com/github/dkw87/chronostt/controller/ManageProjectsController.java
@@ -0,0 +1,23 @@
+package com.github.dkw87.chronostt.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class ManageProjectsController {
+
+    @FXML
+    public VBox projectsContainer;
+
+    @FXML
+    public void addNewRow(MouseEvent event) {
+    }
+
+    @FXML
+    public void cancel() {
+    }
+
+    @FXML
+    public void save() {
+    }
+}
