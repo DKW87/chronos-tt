1541b4cb19dd2ac4990097eb464aa62803de50af
DKW87 <dannykwant@gmail.com>
2026-04-30 19:59:30 +0200

    feat(TrackingController): set listener so actions can be taken on changing value

--- src/main/java/com/github/dkw87/chronostt/controller/TrackingController.java
+++ src/main/java/com/github/dkw87/chronostt/controller/TrackingController.java
@@ -3,6 +3,7 @@ package com.github.dkw87.chronostt.controller;
 import com.github.dkw87.chronostt.StageManager;
 import com.github.dkw87.chronostt.model.Project;
 import com.github.dkw87.chronostt.service.ProjectsService;
+import com.github.dkw87.chronostt.service.TrackingDaysService;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
@@ -23,10 +24,12 @@ public class TrackingController {
     private void initialize() {
         draggableStage();
         initProjectComboBox();
+        setListeners();
     }
 
     @FXML
     private void stopTracking() {
+        TrackingDaysService.getInstance().stopTracking();
         StageManager.getInstance().showSettingsView();
     }
 
@@ -60,6 +63,17 @@ public class TrackingController {
             StageManager.getInstance().getTrackingStage().setX(event.getScreenX() - xOffset);
             StageManager.getInstance().getTrackingStage().setY(event.getScreenY() - yOffset);
         });
+    }
+
+    private void setListeners() {
+        projectComboBox.valueProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue == null) return;
+            startTracking();
+        });
+    }
+
+    private void startTracking() {
+        TrackingDaysService.getInstance().startTracking(projectComboBox.getValue());
     }
 
     public void setProjectToTrack(Project project) {
