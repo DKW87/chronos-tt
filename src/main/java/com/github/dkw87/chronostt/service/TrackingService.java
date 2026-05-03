3b0c5587098626221e4d232afc88cf86b78fb02b
DKW87 <dannykwant@gmail.com>
2026-05-03 19:48:34 +0200

    refactor(TrackingService): remove init of today to avoid null on initialization

--- src/main/java/com/github/dkw87/chronostt/service/TrackingService.java
+++ src/main/java/com/github/dkw87/chronostt/service/TrackingService.java
@@ -23,7 +23,6 @@ public class TrackingService {
     private TrackingService() {
         LOG.info("Initializing {}...", CLASS_NAME);
         queue = new LinkedBlockingQueue<>();
-        today = MemoryRepository.getInstance().getToday();
         start();
     }
 
