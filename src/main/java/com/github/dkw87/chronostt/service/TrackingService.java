6c4082cc799d3933d6f2a413304fc21effe8adcf
DKW87 <dannykwant@gmail.com>
2026-05-05 10:35:52 +0200

    fix(TrackingService): only add secondsWorked for billable projects

--- src/main/java/com/github/dkw87/chronostt/service/TrackingService.java
+++ src/main/java/com/github/dkw87/chronostt/service/TrackingService.java
@@ -95,10 +95,15 @@ public class TrackingService {
                     .filter(t -> t.getId().equals(latestId))
                     .findFirst()
                     .get();
+            
             today.getTimeEntries().remove(entry);
             entry.setEnd(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
-            final long secondsWorked = ChronoUnit.SECONDS.between(entry.getStart(), entry.getEnd());
-            today.setSecondsWorked(today.getSecondsWorked() + secondsWorked);
+
+            if (entry.getProject().getBillable()) {
+                final long secondsWorked = ChronoUnit.SECONDS.between(entry.getStart(), entry.getEnd());
+                today.setSecondsWorked(today.getSecondsWorked() + secondsWorked);
+            }
+
             today.getTimeEntries().add(entry);
             LOG.info("Stopped tracking {} with id {}", entry.getProject().getName(), entry.getId());
         });
