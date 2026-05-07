522091fa88abb41031e62cab81424ce49d103a69
DKW87 <dannykwant@gmail.com>
2026-05-07 20:09:39 +0200

    refactor(ManageProjectsController): add delete icon, give OnMouseClicked and put icon+checkbox in wrappers (for spacing)

--- src/main/java/com/github/dkw87/chronostt/controller/ManageProjectsController.java
+++ src/main/java/com/github/dkw87/chronostt/controller/ManageProjectsController.java
@@ -4,10 +4,12 @@ import com.github.dkw87.chronostt.StageManager;
 import com.github.dkw87.chronostt.model.Project;
 import com.github.dkw87.chronostt.service.ProjectsService;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 public class ManageProjectsController {
 
@@ -35,16 +37,28 @@ public class ManageProjectsController {
 
     private void addRow(Project project) {
         TextField nameField = new TextField(project.getName());
-        nameField.setPrefWidth(150);
+        nameField.setPrefWidth(180);
 
         TextField afasCodeField = new TextField(project.getAfasCode());
         afasCodeField.setPrefWidth(100);
 
         CheckBox billableCheckBox = new CheckBox();
         billableCheckBox.setSelected(project.getBillable());
-        billableCheckBox.setPrefWidth(60);
 
-        HBox row = new HBox(8, nameField, afasCodeField, billableCheckBox);
+        HBox checkboxWrapper = new HBox(billableCheckBox);
+        checkboxWrapper.setPrefWidth(60);
+        checkboxWrapper.setAlignment(Pos.BASELINE_CENTER);
+
+        FontIcon deleteIcon = new FontIcon();
+        deleteIcon.setIconLiteral("mdi2t-trash-can-outline");
+        deleteIcon.setIconSize(20);
+
+        HBox iconWrapper = new HBox(deleteIcon);
+        iconWrapper.setPrefWidth(60);
+        iconWrapper.setAlignment(Pos.BASELINE_CENTER);
+
+        HBox row = new HBox(8, nameField, afasCodeField, checkboxWrapper, iconWrapper);
+        deleteIcon.setOnMouseClicked(e -> projectsContainer.getChildren().remove(row));
         projectsContainer.getChildren().add(row);
     }
 
