75c61da2d1258b9742ff91e7efb4b45d0020098d
DKW87 <dannykwant@gmail.com>
2026-05-08 20:11:50 +0200

    feat(ManageProjectsController): call storeprojects and close manageprojectsview

--- src/main/java/com/github/dkw87/chronostt/controller/ManageProjectsController.java
+++ src/main/java/com/github/dkw87/chronostt/controller/ManageProjectsController.java
@@ -56,6 +56,8 @@ public class ManageProjectsController {
 
         if (hasInvalidProjects(projects)) return;
 
+        ProjectsService.getInstance().storeProjects(projects);
+        StageManager.getInstance().hideManageProjectsView();
     }
 
     private boolean hasInvalidProjects(List<Project> projects) {
