b01c334cf8586fa9d01880209dc3febeefea05d4
DKW87 <dannykwant@gmail.com>
2026-05-08 20:09:51 +0200

    feat(ProjectService): add method store projects that sets IDs for new projects

--- src/main/java/com/github/dkw87/chronostt/service/ProjectsService.java
+++ src/main/java/com/github/dkw87/chronostt/service/ProjectsService.java
@@ -1,6 +1,7 @@
 package com.github.dkw87.chronostt.service;
 
 import com.github.dkw87.chronostt.model.Project;
+import com.github.dkw87.chronostt.model.Settings;
 import com.github.dkw87.chronostt.repository.memory.MemoryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +11,19 @@ import java.util.List;
 public class ProjectsService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProjectsService.class);
+    private static final long NO_ID = -1;
 
     public List<Project> getProjects() {
         return MemoryRepository.getInstance().getProjects();
+    }
+
+    public void storeProjects(List<Project> projects) {
+        projects.forEach(project -> {
+            if (project.getId() == NO_ID) {
+                project.setId(MemoryRepository.getInstance().getIncrementedProjectId());
+            }
+        });
+        MemoryRepository.getInstance().submitProjects(projects);
     }
 
     public static ProjectsService getInstance() {
